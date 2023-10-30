1bb603458b87f1e76203f11ab815d9d65313875a
ParthMakode <makodeparth15@gmail.com>
2023-10-30 13:14:58 +0530

    made component pages initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import "./index.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <p className="text-blue-400">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
-
+import Hero from "./sections/Hero";
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">Super Quality</section>
+    <section className="padding padding-x-10 ">Services</section>
+  </main>
+);
 export default App;
